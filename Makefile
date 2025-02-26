1f6304af23c02cfebeee75ab79cf737d3dfe82be
Pershikov <kartox_a@mail.ru>
2025-02-26 19:58:11 +0000

    dz3

---
+++ Makefile
@@ -0,0 +1,40 @@
+CC      = gcc
+CXX     = g++
+CFLAGS  = -Wall -Wextra -O2
+CXXFLAGS= -Wall -Wextra -O2
+
+MAIN_SRC        = main.c
+UNIT_TEST_SRC   = tests/unit/unit-test.cpp
+
+APP_EXE         = app.exe
+UNIT_TEST_EXE   = unit-tests.exe
+
+GTEST_LIBS      = -lgtest -lgtest_main -pthread
+
+.PHONY: 
+	all clean run-int run-float run-unit-test venv run-integration-tests
+
+all: $(APP_EXE) $(UNIT_TEST_EXE)
+
+$(APP_EXE): $(MAIN_SRC)
+	$(CC) $(CFLAGS) $(MAIN_SRC) -o $(APP_EXE)
+
+$(UNIT_TEST_EXE): $(MAIN_SRC) $(UNIT_TEST_SRC)
+	mkdir -p build 
+	$(CXX) -std=c++14 $(CXXFLAGS) -DUNIT_TEST -I. $(UNIT_TEST_SRC) $(MAIN_SRC) -o $(UNIT_TEST_EXE) $(GTEST_LIBS)
+
+run-int: $(APP_EXE)
+	./$(APP_EXE)
+
+run-float: $(APP_EXE)
+	./$(APP_EXE) --float
+
+run-unit-test: $(UNIT_TEST_EXE)
+	./$(UNIT_TEST_EXE)
+
+format:
+	clang-format -i *.c *.h tests/unit/*.cpp
+
+clean:
+	rm -f $(APP_EXE) $(UNIT_TEST_EXE)
+	rm -rf venv
--- main.c
+++ main.c
@@ -1,117 +1,235 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h> 
+#include <string.h>
 
 #define BUFSIZE 4096
 
 static const char *src;
 
-int parse_grammar(void);
-int parse_division(void);
-int parse_factor(void);
+int parse_number_int(void);
+int parse_factor_int(void);
+int parse_division_int(void);
+int parse_grammar_int(void);
 
-int parse_number(void) {
-    while (isspace(*src))
-        src++;
-    
-    char *endptr;
-    long number = strtol(src, &endptr, 10);
-    if (src == endptr) 
-    {
-        fprintf(stderr, "Error: ожидалось число, а получено: '%c'\n", *src);
-        exit(EXIT_FAILURE);
-    }
-    src = endptr;
-    return (int)number;
+double parse_number_double(void);
+double parse_factor_double(void);
+double parse_division_double(void);
+double parse_grammar_double(void);
+
+int parse_number_int(void) {
+  while (isspace(*src))
+    src++;
+  char *endptr;
+  long number = strtol(src, &endptr, 10);
+  if (src == endptr) {
+    fprintf(stderr, "Error: ожидалось число, а получено '%c'\n", *src);
+    exit(EXIT_FAILURE);
+  }
+  src = endptr;
+  return (int)number;
 }
 
-int parse_factor(void) {
+int parse_factor_int(void) {
+  while (isspace(*src))
+    src++;
+  int result = 0;
+  if (*src == '(') {
+    src++;
+    result = parse_grammar_int();
     while (isspace(*src))
-        src++;
-    
-    int result = 0;
-    if (*src == '(') 
-    {
-        src++;  
-        result = parse_grammar();
-        while (isspace(*src))
-            src++;
-        if (*src == ')')
-            src++;  
-        else 
-        {
-            fprintf(stderr, "Error: ожидалась ')', а получено '%c'\n", *src);
-            exit(EXIT_FAILURE);
+      src++;
+    if (*src == ')')
+      src++;
+    else {
+      fprintf(stderr, "Error: ожидалась ')', а получено '%c'\n", *src);
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    result = parse_number_int();
+  }
+  return result;
+}
+
+int parse_division_int(void) {
+  int result = parse_factor_int();
+  while (1) {
+    while (isspace(*src))
+      src++;
+    if (*src == '*' || *src == '/') {
+      char op = *src;
+      src++;
+      int rhs = parse_factor_int();
+      if (op == '*')
+        result *= rhs;
+      else {
+        if (rhs == 0) {
+          fprintf(stderr, "Error: деление на ноль.\n");
+          exit(EXIT_FAILURE);
         }
-    } else 
-        result = parse_number();
-    return result;
+        result /= rhs;
+      }
+    } else {
+      break;
+    }
+  }
+  return result;
 }
 
-
-int parse_division(void) {
-    int result = parse_factor();
-    while (1) 
-    {
-        while (isspace(*src))
-            src++;
-        if (*src == '*' || *src == '/') 
-        {
-            char op = *src;
-            src++;
-            int rhs = parse_factor();
-            if (op == '*')
-                result *= rhs;
-            else 
-            {
-                if (rhs == 0) 
-                {
-                    fprintf(stderr, "Error: деление на ноль.\n");
-                    exit(EXIT_FAILURE);
-                }
-                result /= rhs;
-            }
-        } else
-            break;
+int parse_grammar_int(void) {
+  int result = parse_division_int();
+  while (1) {
+    while (isspace(*src))
+      src++;
+    if (*src == '+' || *src == '-') {
+      char op = *src;
+      src++;
+      int rhs = parse_division_int();
+      if (op == '+')
+        result += rhs;
+      else
+        result -= rhs;
+    } else {
+      break;
     }
-    return result;
+  }
+  return result;
 }
 
-
-int parse_grammar(void) {
-    int result = parse_division();
-    while (1) {
-        while (isspace(*src))
-            src++;
-        if (*src == '+' || *src == '-') 
-        {
-            char op = *src;
-            src++;
-            int rhs = parse_division();
-            if (op == '+')
-                result += rhs;
-            else
-                result -= rhs;
-        } else
-            break;
-    }
-    return result;
+double parse_number_double(void) {
+  while (isspace(*src))
+    src++;
+  char *endptr;
+  double number = strtod(src, &endptr);
+  if (src == endptr) {
+    fprintf(stderr, "Error: ожидалось число, а получено '%c'\n", *src);
+    exit(EXIT_FAILURE);
+  }
+  src = endptr;
+  return number;
 }
 
-int main(void) {
-    char buffer[BUFSIZE];
-    
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) 
-    {
-        fprintf(stderr, "Error: не удалось прочитать ввод.\n");
-        exit(EXIT_FAILURE);
+double parse_factor_double(void) {
+  while (isspace(*src))
+    src++;
+  double result = 0.0;
+  if (*src == '(') {
+    src++;
+    result = parse_grammar_double();
+    while (isspace(*src))
+      src++;
+    if (*src == ')')
+      src++;
+    else {
+      fprintf(stderr, "Error: ожидалась ')', а получено '%c'\n", *src);
+      exit(EXIT_FAILURE);
     }
-    
-    src = buffer;
-    
-    int result = parse_grammar();
-    
+  } else {
+    result = parse_number_double();
+  }
+  return result;
+}
+
+double parse_division_double(void) {
+  double result = parse_factor_double();
+  while (1) {
+    while (isspace(*src))
+      src++;
+    if (*src == '*' || *src == '/') {
+      char op = *src;
+      src++;
+      double rhs = parse_factor_double();
+      if (op == '*')
+        result *= rhs;
+      else {
+        if (rhs == 0.0) {
+          fprintf(stderr, "Error: деление на ноль.\n");
+          exit(EXIT_FAILURE);
+        }
+        result /= rhs;
+      }
+    } else {
+      break;
+    }
+  }
+  return result;
+}
+
+double parse_grammar_double(void) {
+  double result = parse_division_double();
+  while (1) {
+    while (isspace(*src))
+      src++;
+    if (*src == '+' || *src == '-') {
+      char op = *src;
+      src++;
+      double rhs = parse_division_double();
+      if (op == '+')
+        result += rhs;
+      else
+        result -= rhs;
+    } else {
+      break;
+    }
+  }
+  return result;
+}
+
+void validate_input(const char *input) {
+  size_t len = strlen(input);
+  if (len >= 1024) {
+    fprintf(stderr, "Error: размер ввода превышает 1KiB.\n");
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < len; i++) {
+    char c = input[i];
+    if (!(isdigit(c) || c == '(' || c == ')' || c == '+' || c == '-' ||
+          c == '*' || c == '/' || isspace(c))) {
+      fprintf(stderr, "Error: недопустимый символ '%c' в вводе.\n", c);
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
+#ifndef UNIT_TEST
+int main(int argc, char *argv[]) {
+  int float_mode = 0;
+
+  if (argc > 1 && strcmp(argv[1], "--float") == 0) {
+    float_mode = 1;
+  }
+
+  char buffer[BUFSIZE];
+  if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+    fprintf(stderr, "Error: не удалось прочитать ввод.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  validate_input(buffer);
+
+  src = buffer;
+
+  if (float_mode) {
+    double result = parse_grammar_double();
+    while (isspace(*src))
+      src++;
+    if (*src != '\0' && *src != '\n') {
+      fprintf(stderr,
+              "Error: обнаружены лишние символы после выражения: '%s'\n", src);
+      exit(EXIT_FAILURE);
+    }
+    printf("%f\n", result);
+  } else {
+    int result = parse_grammar_int();
+    while (isspace(*src))
+      src++;
+    if (*src != '\0' && *src != '\n') {
+      fprintf(stderr,
+              "Error: обнаружены лишние символы после выражения: '%s'\n", src);
+      exit(EXIT_FAILURE);
+    }
     printf("%d\n", result);
-    return 0;
+  }
+  return 0;
 }
+#endif
